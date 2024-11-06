28de8d3d500541bdf11e874d4f7323482c4e5713
Julien Denizot <luunz71@gmail.com>
2024-11-06 15:59:06 +0100

    fix server.py

--- server.py
+++ server.py
@@ -24,34 +24,40 @@ clubs = loadClubs()
 
 
 @app.route('/')
-def index():    return render_template('index.html', clubs=clubs)
+def index(): return render_template('index.html', clubs=clubs)
 
 
 @app.route('/showSummary', methods=['POST'])
 def showSummary():
-        try:
-            club = [
-                club for club in clubs
-                if club['email'] == request.form['email']
-            ][0]
-            return render_template(
-                'welcome.html', club=club, competitions=competitions
-            )
+    try:
+        club = [
+            club for club in clubs
+            if club['email'] == request.form['email']
+        ][0]
+        return render_template(
+            'welcome.html', club=club, competitions=competitions
+        )
 
-        except IndexError:
-            flash("Sorry, that email wasn't found.")
-            return redirect(url_for('index'))
+    except IndexError:
+        flash("Sorry, that email wasn't found.")
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
 def book(competition, club):
-    competition = next((c for c in competitions if c['name'] == competition), None)
+    competition = next(
+        (c for c in competitions if c['name'] == competition), None
+    )
     club = next((c for c in clubs if c['name'] == club), None)
 
-    competition_date = datetime.strptime(competition['date'], "%Y-%m-%d %H:%M:%S")
+    competition_date = datetime.strptime(
+        competition['date'], "%Y-%m-%d %H:%M:%S"
+    )
     if competition_date < datetime.now():
         flash("You cannot book places for a past competition.")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template(
+            'welcome.html', club=club, competitions=competitions
+        )
 
     return render_template('booking.html', club=club, competition=competition)
 
@@ -86,9 +92,6 @@ def purchasePlaces():
     return render_template(
         'welcome.html', club=club, competitions=competitions
     )
-
-
-# TODO: Add route for points display
 
 
 @app.route('/logout')
