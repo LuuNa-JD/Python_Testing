1759ffdbedc85b26c30eb6d2cdd9bf023ff7e613
Julien Denizot <luunz71@gmail.com>
2024-11-15 09:04:28 +0100

    fix the directory error of pytest

--- tests/integration/test_booking_for_past_competition.py
+++ tests/integration/test_booking_for_past_competition.py
@@ -1,13 +1,6 @@
-import sys
-import os
 import pytest
 import json
-sys.path.insert(
-    0, os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../../')
-    )
-)
-from server import app, clubs, competitions # noqa
+from server import app, clubs, competitions
 
 
 @pytest.fixture
--- tests/unit/test_points_display.py
+++ tests/unit/test_points_display.py
@@ -1,13 +1,5 @@
-import sys
-import os
 import pytest
-sys.path.insert(
-    0, os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '../../')
-    )
-)
-
-from server import app, clubs # noqa
+from server import app, clubs
 
 
 @pytest.fixture
